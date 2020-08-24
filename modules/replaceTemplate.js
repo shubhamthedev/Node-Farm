74782d0184944f71b891b738f2d7282bcbcdde86
Carter Snook <cartersnook04@gmail.com>
2020-08-23 19:16:39 -0500

    Destructured product
    I would consider a change of the organic property on the product from organic to isOrganic for better practice because it is a boolean.  Also, I would rename the productName property to name as it is redundant as we already know that it is part of the product variable and you don't include product in front of any of the other properties, so it adds inconsistency.  Sorry for the run-on sentences, my teachers would kill me.

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -1,13 +1,13 @@
-module.exports = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) {
+module.exports = (temp, { productName, image, price, from, nutrients, quantity, description, id, organic }) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, productName);
+  output = output.replace(/{%IMAGE%}/g, image);
+  output = output.replace(/{%PRICE%}/g, price);
+  output = output.replace(/{%FROM%}/g, from);
+  output = output.replace(/{%NUTRIENTS%}/g, nutrients);
+  output = output.replace(/{%QUANTITY%}/g, quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, description);
+  output = output.replace(/{%ID%}/g, id);
+  if (!organic) {
     output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
   }
   return output;
